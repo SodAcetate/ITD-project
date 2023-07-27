a16553bd56a1b5ae278fb2d5f55354371f57d4cc
lda22u232 <limonaevda@student.bmstu.ru>
2023-07-27 18:12:38 +0300

    Небольшие исправления dbHandler.go

--- app/dbLogic/dbHandler.go
+++ app/dbLogic/dbHandler.go
@@ -9,9 +9,9 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-type dbhandler struct {
-	conn     pgx.Conn
-	conninfo string
+type DbHandler struct {
+	Conn     pgx.Conn
+	Conninfo string
 }
 
 // получает на вход объект entry (EntryItem или EntryUser)
@@ -21,7 +21,7 @@ type dbhandler struct {
 // Исключение -- логика работы с состояниями
 
 // сгенерить предмет-заглушку
-func (db *dbhandler) GetPlaceholderItem() entry.EntryItem {
+func (db *DbHandler) GetPlaceholderItem() entry.EntryItem {
 	var placeholderItem entry.EntryItem
 	placeholderItem.ID = 1
 	placeholderItem.Name = "PlaceholderItem"
@@ -30,7 +30,7 @@ func (db *dbhandler) GetPlaceholderItem() entry.EntryItem {
 }
 
 // сгенерить юзера-заглушку
-func (db *dbhandler) GetPlaceholderUser() entry.EntryUser {
+func (db *DbHandler) GetPlaceholderUser() entry.EntryUser {
 	var placeholderUser entry.EntryUser
 	placeholderUser.ID = 1
 	placeholderUser.Name = "PlaceholderUsername"
@@ -38,21 +38,21 @@ func (db *dbhandler) GetPlaceholderUser() entry.EntryUser {
 	return placeholderUser
 }
 
-func (db *dbhandler) ConnectDB() {
-	con, err := pgx.Connect(context.Background(), db.conninfo)
+func (db *DbHandler) Init() {
+	con, err := pgx.Connect(context.Background(), db.Conninfo)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "connection to db failed: %v\n", err)
 	}
-	db.conn = *con
+	db.Conn = *con
 }
 
-func (db *dbhandler) CloseDB(conn *pgx.Conn) {
-	conn.Close(context.Background())
+func (db *DbHandler) Deinit() {
+	db.Conn.Close(context.Background())
 }
 
-func (db *dbhandler) GetUserState(ID int64) string {
+func (db *DbHandler) GetUserState(ID int64) string {
 	var state string
-	err := db.conn.QueryRow(context.Background(), fmt.Sprintf("SELECT state FROM users WHERE user_id = %d", ID)).Scan(&state)
+	err := db.Conn.QueryRow(context.Background(), fmt.Sprintf("SELECT state FROM users WHERE user_id = %d", ID)).Scan(&state)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 	}
@@ -60,13 +60,13 @@ func (db *dbhandler) GetUserState(ID int64) string {
 	//return "start"
 }
 
-func (db *dbhandler) UpdateUserState(ID int64, new_state string) error {
-	_, err := db.conn.Exec(context.Background(), "UPDATE users SET state=$1 WHERE user_id=$2", new_state, ID)
+func (db *DbHandler) UpdateUserState(ID int64, new_state string) error {
+	_, err := db.Conn.Exec(context.Background(), "UPDATE users SET state=$1 WHERE user_id=$2", new_state, ID)
 	return err
 }
 
-func (db *dbhandler) AddItem(item entry.EntryItem) error {
-	row := db.conn.QueryRow(context.Background(), "INSERT INTO items (user_id, description) VALUES ($1, $2) RETURNING ID", item.UserInfo.ID, item.Name)
+func (db *DbHandler) AddItem(item entry.EntryItem) error {
+	row := db.Conn.QueryRow(context.Background(), "INSERT INTO items (user_id, description) VALUES ($1, $2) RETURNING ID", item.UserInfo.ID, item.Name)
 	var id int64
 	err := row.Scan(&id)
 	return err
@@ -74,22 +74,22 @@ func (db *dbhandler) AddItem(item entry.EntryItem) error {
 	//err = db.conn.QueryRow(context.Background(), "SELECT state FROM users WHERE user_id = $1", ID).Scan(&state)
 }
 
-func (db *dbhandler) AddUser(item entry.EntryUser) error {
-	row := db.conn.QueryRow(context.Background(), "INSERT INTO users (user_id, user-name, contacts) VALUES ($1, $2, $3) RETURNING user_id", item.ID, item.Name, item.Contact)
+func (db *DbHandler) AddUser(item entry.EntryUser) error {
+	row := db.Conn.QueryRow(context.Background(), "INSERT INTO users (user_id, user-name, contacts) VALUES ($1, $2, $3) RETURNING user_id", item.ID, item.Name, item.Contact)
 	var id int64
 	err := row.Scan(&id)
 	return err
 }
 
-func (db *dbhandler) EditItem(item entry.EntryItem) (entry.EntryItem, error) {
+func (db *DbHandler) EditItem(item entry.EntryItem) (entry.EntryItem, error) {
 	return item, nil
 }
 
-func (db *dbhandler) DeleteItem(item entry.EntryItem) (entry.EntryItem, error) {
+func (db *DbHandler) DeleteItem(item entry.EntryItem) (entry.EntryItem, error) {
 	return item, nil
 }
 
-func (db *dbhandler) GetAll() ([]entry.EntryItem, error) {
+func (db *DbHandler) GetAll() ([]entry.EntryItem, error) {
 
 	items := make([]entry.EntryItem, 0)
 	items = append(items, db.GetPlaceholderItem())
